0e90f4c43edf10ce27f309fe1c804885eead7da0
Aleksey <epsilonalexman@gmail.com>
2022-08-12 22:36:21 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,156 @@
+let scoreBlock
+let score = 0
+const startBtn = document.querySelector('.start_btn')
+const screens = document.querySelectorAll('.screen')
+
+const config = {
+    step: 0,
+    maxStep: 6,
+    sizeCell: 16,
+    sizeBerry: 16 / 4 
+}
+
+const snake = { 
+    x: 16,
+    y: 16,
+    dx: config.sizeCell,
+    dy: 0,
+    tails: [],
+    maxTails: 3 
+}
+
+let berry = {
+    x: 0, 
+    y: 0
+}
+
+let canvas = document.querySelector('#game-canvas')
+let context = canvas.getContext('2d')
+scoreBlock = document.querySelector('.game-score .score-count')
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop)
+    if(++config.step < config.maxStep) {
+        return
+    }
+    config.step = 0
+
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    drawBerry()
+    drawSnake()
+}
+
+function drawSnake() {
+    snake.x += snake.dx
+    snake.y += snake.dy
+
+    collisionBorder()
+
+    snake.tails.unshift({x: snake.x, y: snake.y})
+
+    if (snake.tails.length > snake.maxTails) {
+        snake.tails.pop()
+    }
+
+    snake.tails.forEach((el, index) => {
+        if (index == 0) {
+            context.fillStyle = 'yellow'
+        } else {
+            context.fillStyle = 'yellowgreen'
+        }
+        context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell)
+
+        if(el.x === berry.x && el.y === berry.y) {
+            snake.maxTails++
+            incScore()
+            randomPositionBerry()
+        }
+
+        for(let i = index + 1; i < snake.tails.length; i++) {
+            if(el.x == snake.tails[i].x && el.y == snake.tails[i].y) {
+                refreshGame()
+            }
+        }
+
+    })
+}
+
+function collisionBorder() {
+    if(snake.x < 0) {
+        snake.x = canvas.width - config.sizeCell
+    }else if (snake.x >= canvas.width) {
+        snake.x = 0
+    }
+
+    if(snake.y < 0) {
+        snake.y = canvas.height - config.sizeCell
+    }else if (snake.y >= canvas.height) {
+        snake.y = 0
+    }
+}
+
+function refreshGame() {
+    score = 0
+    drawScore()
+    
+    snake.x = 160
+    snake.y = 160
+    snake.tails = []
+    snake.maxTails = 3
+    snake.dx = config.sizeCell
+    snake.dy = 0
+
+
+}
+
+function drawBerry() {
+    context.beginPath()
+    context.fillStyle = '#fff'
+    context.arc(berry.x + (config.sizeCell / 2), berry.y + (config.sizeCell / 2), config.sizeBerry, 0, 2 * Math.PI)
+    context.fill()
+}
+
+function randomPositionBerry() {
+    berry.x = gerRandomInt(0, canvas.width / config.sizeCell) * config.sizeCell
+    berry.y = gerRandomInt(0, canvas.height / config.sizeCell) * config.sizeCell
+}
+
+function incScore () {
+    score++
+    drawScore()
+}
+
+function drawScore() {
+    scoreBlock.innerHTML = score
+}
+
+function gerRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min )
+}
+
+document.addEventListener('keydown', (e) => {
+    if(e.code == 'KeyW' || e.code == 'ArrowUp'){
+        snake.dy = -config.sizeCell
+        snake.dx = 0
+    }else if(e.code == 'KeyA' || e.code == 'ArrowLeft') {
+        snake.dx = -config.sizeCell
+        snake.dy = 0
+    }else if(e.code == 'KeyS' || e.code == 'ArrowDown') {
+        snake.dy = config.sizeCell
+        snake.dx = 0
+    }else if(e.code == 'KeyD' || e.code == 'ArrowRight') {
+        snake.dx = config.sizeCell
+        snake.dy = 0
+    }
+})
+
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    screens[0].classList.add('up')
+    drawScore()
+    setTimeout(() => {
+        requestAnimationFrame(gameLoop)
+    }, 1000)
+    
+})
